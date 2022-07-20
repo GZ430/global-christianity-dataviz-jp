ffed5c3c6ee68333ff0cce2ae75a1b6348766dac
GZ430 <82852740+GZ430@users.noreply.github.com>
2022-07-20 16:00:10 -0400

    updated the interactive map sections
    Signed-off-by: GZ430 <82852740+GZ430@users.noreply.github.com>

--- demo_code/test_demo.R
+++ demo_code/test_demo.R
@@ -27,12 +27,20 @@ library(shinythemes)
 library(lubridate)
 library(maps)
 library(png)
+library(RColorBrewer)
+library(lattice)
+
+set.seed(100)
 
 #load data 
 countries <- read_csv("https://raw.githubusercontent.com/GZ430/global-christianity-dataviz-jp/main/data/all_countries.csv")
 countries |> clean_names()-> countries
 
-
+all_peoples <- read_csv("https://raw.githubusercontent.com/GZ430/global-christianity-dataviz-jp/main/data/all_peoples_by_country.csv")
+all_peoples |> clean_names() -> all_peoples
+all_peoples$latitude <- as.numeric(all_peoples$latitude)
+all_peoples$longitude <- as.numeric(all_peoples$longitude)
+sample_data <- all_peoples[sample(nrow(all_peoples), size = 300), ]
 
 #change select choices name here:
 vars_name <- c(
@@ -84,7 +92,8 @@ vars_name <- c(
             h6("______________________________________________________"),
             h1(""),
             h5("This project is ongoing. Visit our Github page for more info."),
-            a(href="https://github.com/GZ430/global-christianity-dataviz-jp", "Click here for the GitHub repo!")
+            a(href="https://github.com/GZ430/global-christianity-dataviz-jp", 
+              "Click here for GitHub repo!")
           )
         )
       ),
@@ -99,12 +108,25 @@ vars_name <- c(
           sidebarLayout(
             sidebarPanel(
               h4(
-                "Add input widgets", 
+                "Explore by Religion", 
                 style = "padding-bottom: 20px"
-              )
+              ),
+              selectInput("religion", 
+                          "Select Primary Religion", 
+                          unique(all_peoples$primary_religion)),
+              actionButton("shuffle", "Click Me to Re-Shuffle"),
+              h5("The dots you see on the map represent a random sampling of all 17400 people groups. 
+                 The re-shuffle function is coming soon."),
+              h5(a(href = "https://joshuaproject.net/global/progress", 
+                   "What does each color represent? "))
+              
             ),
             mainPanel(
-              leafletOutput("map_1", width="1000", height="600")
+              h5("Click on the dot to see more information about that group."),
+              leafletOutput("map_religion", width="800", height="400"),
+              h5("The table contains all people groups of the selected religion:"),
+              dataTableOutput("table_religion")
+              
             )
           )
         ),
@@ -113,12 +135,23 @@ vars_name <- c(
           sidebarLayout(
             sidebarPanel(
               h4(
-                "Add input widgets", 
+                "Explore by Continent", 
                 style = "padding-bottom: 20px"
-              )
+              ),
+              selectInput("continent", 
+                          "Select Continent", 
+                          unique(all_peoples$continent)),
+              actionButton("shuffle", "Click Me to Re-Shuffle"),
+              h5("The dots you see on the map represent a random sampling of all 17400 people groups. 
+                 The re-shuffle function is coming soon."),
+              h5(a(href = "https://joshuaproject.net/global/progress", 
+                   "What does each color represent? "))
             ),
             mainPanel(
-              leafletOutput("map_2", width="1000", height="600")
+              h5("Click on the dot to see more information about that group."),
+              leafletOutput("map_continent", width="800", height="400"),
+              h5("The table contains all people groups of the selected continent: "),
+              dataTableOutput("table_continent")
             )
           )
         )
@@ -203,12 +236,74 @@ vars_name <- c(
 #### Server Function #####
 ########################## 
 server <- function(input, output) {
-   
-          output$map_1 <- renderLeaflet({
-            leaflet()|> addTiles()
+  pal <- colorFactor(pal = c("#FF2E18", "#FFCF2E", "#FFEA13", "#B4FF1F","#1BBF3E"), 
+                     domain = c("1", "2", "3", "4", "5"))
+  
+  filtered_data_religion <- reactive({sample_data|> 
+      filter(primary_religion == input$religion)|>
+      select(ctry, peop_name_across_countries, population, jp_scale, longitude, latitude)
+  })
+          output$map_religion <- renderLeaflet({
+            df <- filtered_data_religion()
+            leaflet(data = df) |> addTiles()|> 
+              setView(lng = 30, lat = 30, zoom = 2)|>
+              addCircleMarkers(lng = ~longitude, 
+                         lat = ~latitude,
+                         color = ~pal(jp_scale),
+                         stroke = FALSE,
+                         fillOpacity = 0.6,
+                         popup = paste("Country:", df$ctry, "<br>",
+                                       "People Group Name:", df$peop_name_across_countries, "<br>",
+                                       "JP Scale:", df$jp_scale,"<br>",
+                                       "Population", df$population
+                                       ))|> addLegend(pal=pal, 
+                                                      values=df$jp_scale,
+                                                      opacity = 0.8, 
+                                                      na.label = "Not Available")
+        
           })
-          output$map_2 <- renderLeaflet({
-            leaflet()|> addTiles()
+          
+          output$table_religion <- renderDataTable({
+            df <- all_peoples|> 
+              filter(primary_religion == input$religion)|>
+              select(ctry, peop_name_across_countries, population, jp_scale)
+              colnames(df) <- c("country", "people group name", "population", "JP Scale")
+              print(df)
+            })
+          
+      filtered_data_continent<- reactive({sample_data|> 
+              filter(continent == input$continent)|>
+              select(ctry, peop_name_across_countries, population, jp_scale, longitude, latitude)
+          })
+        
+      
+          output$map_continent <- renderLeaflet({
+            df <- filtered_data_continent()
+            leaflet(data = df) |> addTiles()|> 
+              setView(lng = 30, lat = 30, zoom = 2)|>
+              addCircleMarkers(
+                         lng = ~longitude, 
+                         lat = ~latitude,
+                         color = ~pal(jp_scale),
+                         stroke = FALSE,
+                         fillOpacity = 0.6,
+                         popup = paste("Country:", df$ctry, "<br>",
+                                       "People Group Name:", df$peop_name_across_countries, "<br>",
+                                       "JP Scale:", df$jp_scale,"<br>",
+                                       "Population", df$population))|> 
+              addLegend(pal=pal, 
+                        values=df$jp_scale,
+                        opacity = 0.8, 
+                        na.label = "Not Available")
+            
+          })
+          
+          output$table_continent <- renderDataTable({
+              df <- all_peoples|> 
+              filter(continent == input$continent)|>
+              select(ctry, peop_name_across_countries, population, jp_scale)
+          colnames(df) <- c("country", "people group name", "population", "JP Scale")
+          print(df)
           })
           
           output$table_1 <- renderDataTable(countries)
